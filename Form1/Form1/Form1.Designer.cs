5c4113b9c402cd8de4f1d0d1d78562fcebe321cf
QuangMinh187pm13970 <quangminhkt2000@gmail.com>
2019-12-03 16:28:03 +0700

    Dao Dien Calendar

---
+++ Form1/Form1/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace Calender
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.dtpkDate = new System.Windows.Forms.DateTimePicker();
+            this.btnToDay = new System.Windows.Forms.Button();
+            this.btnMonday = new System.Windows.Forms.Button();
+            this.btnTuesday = new System.Windows.Forms.Button();
+            this.btnWednesday = new System.Windows.Forms.Button();
+            this.btnThusday = new System.Windows.Forms.Button();
+            this.btnFriday = new System.Windows.Forms.Button();
+            this.btnSaturday = new System.Windows.Forms.Button();
+            this.btnSunday = new System.Windows.Forms.Button();
+            this.btnPreviours = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.ckbNotify = new System.Windows.Forms.CheckBox();
+            this.nmNotify = new System.Windows.Forms.NumericUpDown();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nmNotify)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(741, 461);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.panel5);
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Location = new System.Drawing.Point(3, 37);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(735, 421);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.panel6);
+            this.panel3.Controls.Add(this.btnToDay);
+            this.panel3.Controls.Add(this.dtpkDate);
+            this.panel3.Location = new System.Drawing.Point(3, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(735, 28);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.btnNext);
+            this.panel4.Controls.Add(this.btnPreviours);
+            this.panel4.Controls.Add(this.btnSunday);
+            this.panel4.Controls.Add(this.btnSaturday);
+            this.panel4.Controls.Add(this.btnFriday);
+            this.panel4.Controls.Add(this.btnThusday);
+            this.panel4.Controls.Add(this.btnWednesday);
+            this.panel4.Controls.Add(this.btnTuesday);
+            this.panel4.Controls.Add(this.btnMonday);
+            this.panel4.Location = new System.Drawing.Point(3, 3);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(729, 46);
+            this.panel4.TabIndex = 0;
+            // 
+            // panel5
+            // 
+            this.panel5.Location = new System.Drawing.Point(88, 55);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(561, 363);
+            this.panel5.TabIndex = 1;
+            // 
+            // dtpkDate
+            // 
+            this.dtpkDate.Location = new System.Drawing.Point(287, 3);
+            this.dtpkDate.Name = "dtpkDate";
+            this.dtpkDate.Size = new System.Drawing.Size(200, 20);
+            this.dtpkDate.TabIndex = 0;
+            // 
+            // btnToDay
+            // 
+            this.btnToDay.Location = new System.Drawing.Point(493, 0);
+            this.btnToDay.Name = "btnToDay";
+            this.btnToDay.Size = new System.Drawing.Size(75, 23);
+            this.btnToDay.TabIndex = 1;
+            this.btnToDay.Text = "Hôm nay";
+            this.btnToDay.UseVisualStyleBackColor = true;
+            // 
+            // btnMonday
+            // 
+            this.btnMonday.Location = new System.Drawing.Point(85, 3);
+            this.btnMonday.Name = "btnMonday";
+            this.btnMonday.Size = new System.Drawing.Size(75, 40);
+            this.btnMonday.TabIndex = 0;
+            this.btnMonday.Text = "Thứ 2";
+            this.btnMonday.UseVisualStyleBackColor = true;
+            // 
+            // btnTuesday
+            // 
+            this.btnTuesday.Location = new System.Drawing.Point(166, 3);
+            this.btnTuesday.Name = "btnTuesday";
+            this.btnTuesday.Size = new System.Drawing.Size(75, 40);
+            this.btnTuesday.TabIndex = 1;
+            this.btnTuesday.Text = "Thứ 3";
+            this.btnTuesday.UseVisualStyleBackColor = true;
+            // 
+            // btnWednesday
+            // 
+            this.btnWednesday.Location = new System.Drawing.Point(247, 3);
+            this.btnWednesday.Name = "btnWednesday";
+            this.btnWednesday.Size = new System.Drawing.Size(75, 40);
+            this.btnWednesday.TabIndex = 2;
+            this.btnWednesday.Text = "Thứ 4";
+            this.btnWednesday.UseVisualStyleBackColor = true;
+            // 
+            // btnThusday
+            // 
+            this.btnThusday.Location = new System.Drawing.Point(328, 3);
+            this.btnThusday.Name = "btnThusday";
+            this.btnThusday.Size = new System.Drawing.Size(75, 40);
+            this.btnThusday.TabIndex = 3;
+            this.btnThusday.Text = "Thứ 5";
+            this.btnThusday.UseVisualStyleBackColor = true;
+            // 
+            // btnFriday
+            // 
+            this.btnFriday.Location = new System.Drawing.Point(409, 3);
+            this.btnFriday.Name = "btnFriday";
+            this.btnFriday.Size = new System.Drawing.Size(75, 40);
+            this.btnFriday.TabIndex = 4;
+            this.btnFriday.Text = "Thứ 6";
+            this.btnFriday.UseVisualStyleBackColor = true;
+            // 
+            // btnSaturday
+            // 
+            this.btnSaturday.Location = new System.Drawing.Point(490, 3);
+            this.btnSaturday.Name = "btnSaturday";
+            this.btnSaturday.Size = new System.Drawing.Size(75, 40);
+            this.btnSaturday.TabIndex = 5;
+            this.btnSaturday.Text = "Thứ 7";
+            this.btnSaturday.UseVisualStyleBackColor = true;
+            // 
+            // btnSunday
+            // 
+            this.btnSunday.Location = new System.Drawing.Point(571, 3);
+            this.btnSunday.Name = "btnSunday";
+            this.btnSunday.Size = new System.Drawing.Size(75, 40);
+            this.btnSunday.TabIndex = 6;
+            this.btnSunday.Text = "Chủ nhật";
+            this.btnSunday.UseVisualStyleBackColor = true;
+            // 
+            // btnPreviours
+            // 
+            this.btnPreviours.Location = new System.Drawing.Point(4, 3);
+            this.btnPreviours.Name = "btnPreviours";
+            this.btnPreviours.Size = new System.Drawing.Size(75, 40);
+            this.btnPreviours.TabIndex = 7;
+            this.btnPreviours.Text = "Tháng trước";
+            this.btnPreviours.UseVisualStyleBackColor = true;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(651, 3);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 40);
+            this.btnNext.TabIndex = 8;
+            this.btnNext.Text = "Tháng sau";
+            this.btnNext.UseVisualStyleBackColor = true;
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.nmNotify);
+            this.panel6.Controls.Add(this.ckbNotify);
+            this.panel6.Location = new System.Drawing.Point(7, 0);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(210, 28);
+            this.panel6.TabIndex = 2;
+            // 
+            // ckbNotify
+            // 
+            this.ckbNotify.AutoSize = true;
+            this.ckbNotify.Location = new System.Drawing.Point(3, 4);
+            this.ckbNotify.Name = "ckbNotify";
+            this.ckbNotify.Size = new System.Drawing.Size(53, 17);
+            this.ckbNotify.TabIndex = 0;
+            this.ckbNotify.Text = "Notify";
+            this.ckbNotify.UseVisualStyleBackColor = true;
+            // 
+            // nmNotify
+            // 
+            this.nmNotify.Location = new System.Drawing.Point(62, 4);
+            this.nmNotify.Maximum = new decimal(new int[] {
+            3600,
+            0,
+            0,
+            0});
+            this.nmNotify.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nmNotify.Name = "nmNotify";
+            this.nmNotify.Size = new System.Drawing.Size(60, 20);
+            this.nmNotify.TabIndex = 1;
+            this.nmNotify.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(765, 478);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Calender";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nmNotify)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.NumericUpDown nmNotify;
+        private System.Windows.Forms.CheckBox ckbNotify;
+        private System.Windows.Forms.Button btnToDay;
+        private System.Windows.Forms.DateTimePicker dtpkDate;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPreviours;
+        private System.Windows.Forms.Button btnSunday;
+        private System.Windows.Forms.Button btnSaturday;
+        private System.Windows.Forms.Button btnFriday;
+        private System.Windows.Forms.Button btnThusday;
+        private System.Windows.Forms.Button btnWednesday;
+        private System.Windows.Forms.Button btnTuesday;
+        private System.Windows.Forms.Button btnMonday;
+    }
+}
+
